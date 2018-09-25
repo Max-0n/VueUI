3c159136370aa5d3c8742ba3905793e25705d79d
Max <max0n@me.com>
2018-09-25 17:19:49 +0300

    Second little fix :D

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -78,13 +78,8 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+@Component({})
 export default class Home extends Vue {}
 </script>
 
